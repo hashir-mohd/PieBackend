cceb969cbe5b37a073ef92308e80f8b343847a45
one <esport.py.m45@gmail.com>
2025-06-04 09:49:57 +0530

    chore: initialize package.json with basic configuration and dependencies

---
+++ index.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config({
+  path: "./.env",
+});
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+app.on("error", (error) => {
+  console.log("Server Run Failed :", error);
+  throw error;
+});
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`  ⚙️   Server is running at port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed !! ", err);
+  });
+
+app.use(
+  cors({
+    origin:
+        process.env.CORS_ORIGIN || "*",
+        credentials: true,
+  })
+);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
+
+
